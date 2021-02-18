95eb831816b102a74d3c1c16bf4460dde492c9f9
Saravanavel0504 <saravanavel0504@gmail.com>
2021-02-18 12:10:39 +0530

    code second time

--- src/org/cts/Clonning.java
+++ src/org/cts/Clonning.java
@@ -4,8 +4,9 @@ public class Clonning {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome");
-        System.out.println("into shop");
+        System.out.println("ANGRY");
 		System.out.println("buy vegtables");
+		System.out.println("Fruits");
 
 	}
 
